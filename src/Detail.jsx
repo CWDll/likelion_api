f0776d11be3d4e7342100252b7222cf8e7919185
leechangjoon <2018110021@dongguk.edu>
2023-07-31 23:32:25 +0900

    final commit

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -10,20 +10,29 @@ const Container = styled.div`
   margin: auto;
   justify-content: center;
   align-items: center;
-  background-color: lightgreen;
+  background-color: white;
+  border-radius: 20px;
 `;
 
 const Header = styled.div`
   width: 100%;
   height: 50px;
-  border: 1px solid black;
   display: flex;
   justify-content: space-between;
 `;
 
 const Title = styled.h1`
   font-size: 30px;
-  color: red;
+  color: black;
+  font-weight: bold;
+  textalign: center;
+`;
+
+const Rate = styled(Title)`
+  color: blue;
+  font-size: 35px;
+  margin: 10px;
+  font-weight: bold;
 `;
 
 const Text = styled.p`
@@ -37,30 +46,48 @@ const BodyContainer = styled.div`
   width: 100%;
 `;
 
+const RateContiner = styled(BodyContainer)`
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
 const LeftBodyContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: stretch;
-  border: 1px solid blue;
   flex-direction: column;
+  border-radius: 20px;
   width: 40%;
-  height: 300px;
+  height: 200px;
   padding-left: 30px;
+  background-color: lightyellow;
 `;
 
 const RightBodyContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid red;
+  border-radius: 20px;
+  opacity: 0.8;
   textalign: center;
   width: 60%;
   height: 100%;
+  background-color: lightgreen;
 `;
 
 const Detail = () => {
   const location = useLocation();
   const { data } = location.state;
+
+  // MSRDT에서 연도, 월, 일을 추출하여 한글로 표시할 문자열 생성
+  const year = data.MSRDT.substring(0, 4);
+  const month = data.MSRDT.substring(4, 6);
+  const day = data.MSRDT.substring(6, 8);
+  const formattedDate = `${year}년 ${parseInt(month, 10)}월 ${parseInt(
+    day,
+    10
+  )}일`;
 
   return (
     <Container>
@@ -68,18 +95,22 @@ const Detail = () => {
         <Title>
           {data.MSRRGN_NM} | {data.MSRSTE_NM}
         </Title>
-        <Text>측정일: {data.MSRDT}</Text>
+        <Text>측정일: {formattedDate}</Text>
       </Header>
       <BodyContainer>
         <LeftBodyContainer>
           <Text>미세먼지: {data.PM10}</Text>
           <Text>초미세먼지농도: {data.PM25}</Text>
           <Text>오존농도: {data.O3}</Text>
-          <Text>통합대기환경등급: {data.IDEX_NM}</Text>
           <Text>통합대기환경지수: {data.IDEX_MVL}</Text>
         </LeftBodyContainer>
         <RightBodyContainer>
-          <Text>권역: {data.MSRRGN_NM}</Text>
+          <BodyContainer>
+            <RateContiner>
+              <Text>통합대기환경등급</Text>
+              <Rate>{data.IDEX_NM}</Rate>
+            </RateContiner>
+          </BodyContainer>
         </RightBodyContainer>
       </BodyContainer>
       {/* 여기에 다른 정보들도 추가할 수 있습니다. */}
