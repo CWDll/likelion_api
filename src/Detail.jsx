7975364de6a4d7c35b006f692b4d71201d5aea7e
leechangjoon <2018110021@dongguk.edu>
2023-07-31 22:50:15 +0900

    first commit

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,10 +1,40 @@
-// Detail.jsx
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  width: 70%;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  justify-content: center;
+  align-items: center;
+  background-color: green;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: red;
+`;
 
 const Detail = () => {
-  {
-    /* 측정소별 디테일 페이지를 작성해주세요. */
-  }
-  return <>측정소 디테일 페이지</>;
+  const location = useLocation();
+  const { data } = location.state;
+
+  return (
+    <Container>
+      <Title>측정소: {data.MSRSTE_NM}</Title>
+      <p>측정일: {data.MSRDT}</p>
+      <p>미세먼지: {data.PM10}</p>
+      <p>초미세먼지농도: {data.PM25}</p>
+      <p>오존농도: {data.O3}</p>
+      <p>통합대기환경등급: {data.IDEX_NM}</p>
+      <p>통합대기환경지수: {data.IDEX_MVL}</p>
+      <p>권역: {data.MSRRGN_NM}</p>
+      {/* 여기에 다른 정보들도 추가할 수 있습니다. */}
+    </Container>
+  );
 };
 
 export default Detail;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,112 @@
-import { BodySection, HeadSection, HeadText, HomeContainer } from "./components/BodyStyle";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  BodySection,
+  DataTable,
+  HeadSection,
+  HeadText,
+  HomeContainer,
+  TableTd,
+  TableTh,
+} from "./components/BodyStyle";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   // State 작성------------------------------------------------
+  const [weatherData, setWeatherData] = useState([]);
+  const API_KEY = import.meta.env;
+  //비구조화 할당 : import.meta.env 안에 { }안 속 놈과 이름이 같은 게 있으면 바로 할당해준다. 만약 없으면 할당X
+  //gitignore에 env 넣음으로써 인증키 숨김.
+  const { VITE_APP_API_KEY } = import.meta.env;
 
   // Function 작성---------------------------------------------
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `http://openAPI.seoul.go.kr:8088/${VITE_APP_API_KEY}/json/RealtimeCityAir/1/5/`
+      );
+      setWeatherData(response.data.RealtimeCityAir.row);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(weatherData);
 
   // OPEN API 비동기로 불러와 State에 저장하기
 
   // ComponentDidMount-----------------------------------------
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const sortedData = weatherData.sort((a, b) => a.PM10 - b.PM10).slice(0, 3);
+  console.log(sortedData);
+
+  const getTextColor = (idexNm) => {
+    if (idexNm === "좋음") {
+      return "green";
+    } else if (idexNm === "나쁨") {
+      return "red";
+    } else {
+      return ""; // 기본 색상, 원하는 색상으로 설정하거나 특정 색상이 없을 경우 빈 문자열로 남겨둘 수 있습니다.
+    }
+  };
 
   return (
     <HomeContainer>
       <HeadSection>
         <HeadText>서울시 권역별 실시간 대기환경 현황</HeadText>
       </HeadSection>
-      <BodySection>{/* 아래에 날씨 Open API 관련 코드 작성 */}</BodySection>
+      <BodySection>
+        <DataTable>
+          <thead>
+            <tr>
+              <TableTh>측정일</TableTh>
+              <TableTh>측정소</TableTh>
+              <TableTh>미세먼지</TableTh>
+              <TableTh>초미세먼지농도</TableTh>
+              <TableTh>통합대기환경등급</TableTh>
+              <TableTh>통합대기환경지수</TableTh>
+            </tr>
+          </thead>
+          <tbody>
+            {weatherData.map((data, idx) => (
+              <tr
+                key={idx}
+                style={{
+                  backgroundColor:
+                    data.MSRSTE_NM === "중구" ? "orange" : "inherit",
+                }}
+              >
+                <TableTd>{data.MSRDT}</TableTd>
+                <TableTd
+                  onClick={() =>
+                    navigate(`/detail/${data.MSRSTE_NM}`, {
+                      state: {
+                        data: data,
+                      },
+                    })
+                  }
+                >
+                  {data.MSRSTE_NM}
+                </TableTd>
+                <TableTd
+                  style={{ color: sortedData.includes(data) ? "yellow" : "" }}
+                >
+                  {data.PM10}
+                </TableTd>
+                <TableTd>{data.PM25}</TableTd>
+                <TableTd style={{ color: getTextColor(data.IDEX_NM) }}>
+                  {data.IDEX_NM === "점검 중" ? "-" : data.IDEX_NM}
+                </TableTd>
+                <TableTd>{data.IDEX_MVL}</TableTd>
+              </tr>
+            ))}
+          </tbody>
+        </DataTable>
+      </BodySection>
     </HomeContainer>
   );
 };
