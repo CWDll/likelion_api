930f9c8b7e6d79290b3163d97e92d8482f6401b8
leechangjoon <2018110021@dongguk.edu>
2023-07-31 23:09:17 +0900

    [Design] detail page layout]

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -10,12 +10,52 @@ const Container = styled.div`
   margin: auto;
   justify-content: center;
   align-items: center;
-  background-color: green;
+  background-color: lightgreen;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 50px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Title = styled.h1`
-  font-size: 50px;
+  font-size: 30px;
   color: red;
+`;
+
+const Text = styled.p`
+  color: black;
+  font-size: 20px;
+`;
+
+const BodyContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+`;
+
+const LeftBodyContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+  border: 1px solid blue;
+  flex-direction: column;
+  width: 40%;
+  height: 300px;
+  padding-left: 30px;
+`;
+
+const RightBodyContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid red;
+  textalign: center;
+  width: 60%;
+  height: 100%;
 `;
 
 const Detail = () => {
@@ -24,14 +64,24 @@ const Detail = () => {
 
   return (
     <Container>
-      <Title>측정소: {data.MSRSTE_NM}</Title>
-      <p>측정일: {data.MSRDT}</p>
-      <p>미세먼지: {data.PM10}</p>
-      <p>초미세먼지농도: {data.PM25}</p>
-      <p>오존농도: {data.O3}</p>
-      <p>통합대기환경등급: {data.IDEX_NM}</p>
-      <p>통합대기환경지수: {data.IDEX_MVL}</p>
-      <p>권역: {data.MSRRGN_NM}</p>
+      <Header>
+        <Title>
+          {data.MSRRGN_NM} | {data.MSRSTE_NM}
+        </Title>
+        <Text>측정일: {data.MSRDT}</Text>
+      </Header>
+      <BodyContainer>
+        <LeftBodyContainer>
+          <Text>미세먼지: {data.PM10}</Text>
+          <Text>초미세먼지농도: {data.PM25}</Text>
+          <Text>오존농도: {data.O3}</Text>
+          <Text>통합대기환경등급: {data.IDEX_NM}</Text>
+          <Text>통합대기환경지수: {data.IDEX_MVL}</Text>
+        </LeftBodyContainer>
+        <RightBodyContainer>
+          <Text>권역: {data.MSRRGN_NM}</Text>
+        </RightBodyContainer>
+      </BodyContainer>
       {/* 여기에 다른 정보들도 추가할 수 있습니다. */}
     </Container>
   );
